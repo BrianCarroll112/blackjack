a74d512477920df9b17537c79156f75b12b39df3
Brian <brian@ool-ad0205b2.dyn.optonline.net>
2019-02-04 10:10:12 -0500

    pseudo code

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log('linked');
+
+/*
+create deck of card objects and shuffle -- see high/low
+   cards will need img, value,
+
+give dealer 2 cards, one face down.
+  dealer object, player object
+
+give player 2 cards face up
+
+player turn -
+  option to hit/hold
+    hold - pass turn to dealer
+    hit - add one card to player's div - then hit/hold again
+  check if over 21 after every hit. if so, lose.
+
+dealer turn -
+  continues to hit until cards add up to 17 or more.
+  same over 21 check.
+
+post dealer turn -
+  compare scores, decide who won hand.
+  add to "hands won" for dealer/player
+  see if hands won meets win/loss condition
+  start from line 7 OR display win/loss
+
+
+
+*/
